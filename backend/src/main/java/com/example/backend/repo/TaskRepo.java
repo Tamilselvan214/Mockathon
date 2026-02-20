72f19bee4dc8e1ad0773184a216cdfd2e6adb6fc
ajith-30 <Ajith2830@>
2026-02-20 14:47:07 +0530

    added jpa

---
+++ backend/src/main/java/com/example/backend/repo/TaskRepo.java
@@ -0,0 +1,12 @@
+package com.example.backend.repo;
+
+import com.example.backend.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepo extends JpaRepository<Task, UUID> {
+
+}

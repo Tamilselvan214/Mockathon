eec14ab7554d43d630257e398e0bb1c972354e70
darky9197 <darkking9197@gmail.com>
2026-02-20 14:38:48 +0530

    react inital setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const AppContainer = styled.section``;
+function App() {
+  return (
+    <AppContainer>
+      <h1 className="m-auto bg-green-200 h-1/5 w-40">whatever</h1>
+    </AppContainer>
+  );
+}
+
+export default App;
